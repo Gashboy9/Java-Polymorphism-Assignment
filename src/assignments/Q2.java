13219a1ae8c4bb0a315e4f34af91a467b42e3648
gashboy9 <gashboy@yahoo.com>
2021-03-16 11:36:44 +0000

    Q2 completed with sample data

--- src/assignments/Q2.java
+++ src/assignments/Q2.java
@@ -4,7 +4,46 @@ public class Q2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Monster m;
+		m= new FireMonster();
+		System.out.println(m.attack());
+		
+		m = new WaterMonster();
+		System.out.println(m.attack());
+		
+		m = new StoneMonster();
+		System.out.println(m.attack());
 
 	}
 
+}
+
+
+abstract class Monster{
+	
+	private String name;
+	
+	abstract public String attack();
+}
+
+class FireMonster extends Monster{
+	
+	public String attack() {
+		return "Fire Monster";
+	}
+}
+
+class WaterMonster extends Monster{
+	
+	public String attack() {
+		return "Water Monster";
+	}
+}
+
+class StoneMonster extends Monster{
+	
+	public String attack() {
+		return "Stone Monster";
+	}
 }
